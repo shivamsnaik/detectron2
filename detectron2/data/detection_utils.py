0fd0c0ecd6084faa85f9a61e317deb8c26424555
Shivam Naik <shivamsnaik@gmail.com>
2021-12-24 22:47:13 +0100

    Update detection_utils.py
    Annotation Dicts will always take original image shape into consideration

--- detectron2/data/detection_utils.py
+++ detectron2/data/detection_utils.py
@@ -189,26 +189,26 @@ def check_image_size(dataset_dict, image):
     """
     Raise an error if the image does not match the size specified in the dict.
     """
-    if "width" in dataset_dict or "height" in dataset_dict:
-        image_wh = (image.shape[1], image.shape[0])
-        expected_wh = (dataset_dict["width"], dataset_dict["height"])
-        if not image_wh == expected_wh:
-            raise SizeMismatchError(
-                "Mismatched image shape{}, got {}, expect {}.".format(
-                    " for image " + dataset_dict["file_name"]
-                    if "file_name" in dataset_dict
-                    else "",
-                    image_wh,
-                    expected_wh,
-                )
-                + " Please check the width/height in your annotation."
-            )
+#     if "width" in dataset_dict or "height" in dataset_dict:
+#         image_wh = (image.shape[1], image.shape[0])
+#         expected_wh = (dataset_dict["width"], dataset_dict["height"])
+#         if not image_wh == expected_wh:
+#             raise SizeMismatchError(
+#                 "Mismatched image shape{}, got {}, expect {}.".format(
+#                     " for image " + dataset_dict["file_name"]
+#                     if "file_name" in dataset_dict
+#                     else "",
+#                     image_wh,
+#                     expected_wh,
+#                 )
+#                 + " Please check the width/height in your annotation."
+#             )
 
     # To ensure bbox always remap to original image size
-    if "width" not in dataset_dict:
-        dataset_dict["width"] = image.shape[1]
-    if "height" not in dataset_dict:
-        dataset_dict["height"] = image.shape[0]
+#     if "width" not in dataset_dict:
+    dataset_dict["width"] = image.shape[1]
+#     if "height" not in dataset_dict:
+    dataset_dict["height"] = image.shape[0]
 
 
 def transform_proposals(dataset_dict, image_shape, transforms, *, proposal_topk, min_box_size=0):
