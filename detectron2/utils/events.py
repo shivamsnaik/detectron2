eac29c991ed3fc0adb134ba60c31e809cb99ee21
Shivam Naik <shivamsnaik@gmail.com>
2022-01-20 12:06:03 +0100

    Update events.py
    Fix class variable used without self keyword

--- detectron2/utils/events.py
+++ detectron2/utils/events.py
@@ -253,7 +253,7 @@ class CommonMetricPrinter(EventWriter):
             max_mem_mb = None
 
         # NOTE: max_mem is parsed by grep in "dev/parse_results.sh"
-        logger.info(
+        self.logger.info(
             "{{{eta}\"iter\": {iter}, {losses} {time}, {data_time}, \"lr\": {lr}, {memory},}}".format(
                 eta=f"\"eta\": \"{eta_string}\", " if eta_string else "",
                 iter=iteration,
