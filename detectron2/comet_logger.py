50697a696b8ebdc4a85242871870c79590ca2d4c
Shivam Naik <shivamsnaik@gmail.com>
2022-01-20 23:56:33 +0100

    Add ExistingExperiment logic
    Testing: Allows single experiments monitoring with multiple GPUs.

--- detectron2/comet_logger.py
+++ detectron2/comet_logger.py
@@ -7,18 +7,29 @@
 # ==============================================================================
 
 # Import comet_ml at the top of your file
-from comet_ml import Experiment
+from comet_ml import Experiment, ExistingExperiment
 import os
 
 COMET_LOGGER = None
-
+COMET_LOGGER_EXPERIMENT_ID = None
 def init():
     # Init Comet Logger as Global
     global COMET_LOGGER
+    global COMET_LOGGER_EXPERIMENT_ID
 
-    COMET_LOGGER = Experiment(
-        api_key=os.environ['COMET_API_KEY'],
-        project_name="dynamichead",
-        workspace="shivamsnaik",
-        auto_output_logging="simple"
-    )
+    if COMET_LOGGER_EXPERIMENT_ID == None:
+        COMET_LOGGER = Experiment(
+            api_key=os.environ['COMET_API_KEY'],
+            project_name="dynamichead",
+            workspace="shivamsnaik",
+            auto_output_logging="simple"
+        )
+        COMET_LOGGER_EXPERIMENT_ID = COMET_LOGGER.get_key()
+    else:
+        COMET_LOGGER = ExistingExperiment(
+            api_key=os.environ['COMET_API_KEY'],
+            project_name="dynamichead",
+            workspace="shivamsnaik",
+            auto_output_logging="simple",
+            previous_experiment=COMET_LOGGER_EXPERIMENT_ID
+        )
